6cb27f0a85c9ae718045e1925be1de583a304682
skermabon <skermabon@talend.com>
2017-11-09 18:11:03 +0100

    continue unit tests on controller

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cbc
-Comic Books Collection
+# CBC Comic Books Collection
+
+A simple application to test techno like Spring 5.0 / Spring-boot 2.0 / Reactive programmation / ReactJS / Mongo / ...
--- backend/backaend-api/src/test/java/org/demis27/cbc/api/controller/PersonControllerTest.java
+++ backend/backaend-api/src/test/java/org/demis27/cbc/api/controller/PersonControllerTest.java
@@ -1,13 +1,19 @@
 package org.demis27.cbc.api.controller;
 
 import static com.jayway.restassured.RestAssured.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.demis27.cbc.api.dto.Person;
 import org.demis27.cbc.api.service.PersonService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -21,6 +27,7 @@ import org.springframework.util.ReflectionUtils;
 import com.jayway.restassured.response.Response;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,8 +42,8 @@ public class PersonControllerTest {
     @Autowired
     private ApplicationContext context;
 
-    @Test
-    public void testList() throws Exception {
+    @Before
+    public void setup() {
         final Field serviceField = ReflectionUtils.findField(PersonController.class, "service");
         ReflectionUtils.makeAccessible(serviceField);
         ReflectionUtils.setField(serviceField, context.getBean("personController"), service);
@@ -46,31 +53,106 @@ public class PersonControllerTest {
         stan.lastName = "Lee";
         stan.id = "1";
 
-        Flux<Person> result = Flux.just(stan);
-        when(service.findAll()).thenReturn(result);
+        Person jack = new Person();
+        jack.firstName = "Jack";
+        jack.lastName = "Kirby";
+        jack.id = "2";
 
-        Response response = given().port(port).expect().statusCode(200).when().get("/api/person");
+        Person steve = new Person();
+        steve.firstName = "Steve";
+        steve.lastName = "Ditko";
+        steve.id = "3";
+
+        when(service.findAll()).thenReturn(Flux.just(stan, jack));
+        when(service.findById("1")).thenReturn(Mono.just(stan));
+        when(service.create(any(Person.class))).thenReturn(Mono.just(steve));
+        when(service.delete(anyString())).thenReturn(Mono.<Void>empty());
+    }
+
+    @Test
+    public void testList() throws Exception {
+        // @formatter:off
+        Response response = 
+                given()
+                        .port(port)
+                        .expect()
+                        .statusCode(200)
+                .when()
+                        .get("/api/person");
+        // @formatter:on
         Person[] persons = response.getBody().as(Person[].class);
-        Assert.assertEquals(1, persons.length);
+        Assert.assertEquals(2, persons.length);
     }
 
     @Test
     public void testGet() throws Exception {
+        // @formatter:off
+        Response response =
+                given()
+                        .port(port)
+                        .expect()
+                        .statusCode(200)
+                .when()
+                        .get("/api/person/1");
+        // @formatter:on
+        Person person = response.as(Person.class);
+        Assert.assertEquals("Stan", person.firstName);
+        Assert.assertEquals("Lee", person.lastName);
+        Assert.assertEquals("1", person.id);
     }
 
     @Test
     public void testPost() throws Exception {
+        String steve = "{\"firstName\": \"Steve\", \"lastName\": \"Ditko\"}";
+        // @formatter:off
+        Response response =
+                given()
+                        .port(port)
+                        .header("Content-Type", "application/json")
+                        .body(steve)
+                        .expect()
+                        .statusCode(200)
+                .when()
+                        .post("/api/person");
+        // @formatter:on
+
+        Person person = response.as(Person.class);
+        Assert.assertEquals("Steve", person.firstName);
+        Assert.assertEquals("Ditko", person.lastName);
+        Assert.assertEquals("3", person.id);
     }
 
     @Test
     public void testDelete() throws Exception {
+        // @formatter:off
+        Response response =
+                given()
+                        .port(port)
+                        .expect()
+                        .statusCode(200)
+                .when()
+                        .delete("/api/person/1");
+        // @formatter:on
     }
 
     @Test
     public void testOptions() throws Exception {
-        Response response = given().port(port).expect().statusCode(204).when().options("/api/person");
-
-        //Assert.assertEquals("HEAD,GET,POST,OPTIONS,DELETE", response.header("Allow"));
+        // @formatter:off
+        Response response =
+                given()
+                        .port(port)
+                        .expect()
+                        .statusCode(204)
+                .when()
+                        .options("/api/person");
+        // @formatter:on
+        assertMethods("OPTIONS,GET,POST,HEAD,DELETE", response.header("Allow"));
     }
 
+    public void assertMethods(String expected, String actual) {
+        List<String> actualMethods = Arrays.asList(actual.split("\\,", -1)).stream().sorted().collect(Collectors.toList());
+        List<String> expectedMethods = Arrays.asList(expected.split("\\,", -1)).stream().sorted().collect(Collectors.toList());
+
+        Assert.assertEquals(expectedMethods, actualMethods);
+    }
 }
